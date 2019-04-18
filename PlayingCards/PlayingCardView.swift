c251b1761cc7c14edc74eaf86a6df324ca2e40f3
ssh <kathishivaram@gmail.com>
2019-04-18 17:45:25 +0530

    add rank and suit to card

--- PlayingCards/PlayingCardView.swift
+++ PlayingCards/PlayingCardView.swift
@@ -10,14 +10,121 @@ import UIKit
 
 class PlayingCardView: UIView {
 
-   
+    var rank:Int = 5  {didSet{setNeedsDisplay();setNeedsLayout()}}
+    var suit:String = "❤️" {didSet{setNeedsDisplay();setNeedsLayout()}}
+    var isFaceUp:Bool = true {didSet{setNeedsDisplay();setNeedsLayout()}}
+    
+    private func centeredAttributedString(_ string: String,fontSize: CGFloat) -> NSAttributedString {
+        var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
+        font = UIFontMetrics(forTextStyle: .body).scaledFont(for: font) //for scaling on slider
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        
+        return NSAttributedString(string: string, attributes:  [.paragraphStyle : paragraphStyle,.font : font])
+    }
+    
+    private var cornerString: NSAttributedString {
+        return centeredAttributedString(rankString+"\n"+suit , fontSize: cornerFontSize)
+    }
+    
+    private lazy var upperLeftCornerLabel = createCornerLabel()
+    private lazy var lowerRightCornerLabel = createCornerLabel()
+    
+    private func createCornerLabel() -> UILabel {
+        let label = UILabel()
+        label.numberOfLines = 0
+        addSubview(label)
+        return label
+    }
+    private func configureCornerlabel(_ label: UILabel) {
+        label.attributedText = cornerString
+        label.frame.size = CGSize.zero
+        label.sizeToFit()
+        label.isHidden = !isFaceUp
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        setNeedsDisplay()
+        setNeedsLayout()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        configureCornerlabel(upperLeftCornerLabel)
+        configureCornerlabel(lowerRightCornerLabel)
+        upperLeftCornerLabel.frame.origin = bounds.origin.offsetBy(dx: cornerOffset, dy: cornerOffset)
+        lowerRightCornerLabel.transform = CGAffineTransform.identity
+            .translatedBy(x: lowerRightCornerLabel.frame.size.width, y: lowerRightCornerLabel.frame.size.height)
+            .rotated(by: CGFloat.pi)
+        lowerRightCornerLabel.frame.origin = CGPoint(x: bounds.maxX, y: bounds.maxY)
+            .offsetBy(dx: -cornerOffset, dy: -cornerOffset)
+            .offsetBy(dx: -lowerRightCornerLabel.frame.width, dy: -lowerRightCornerLabel.frame.height)
+    }
+    
     override func draw(_ rect: CGRect) {
         
-        let rounderRect = UIBezierPath(roundedRect: bounds, cornerRadius: 16.0)
+        let rounderRect = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
         rounderRect.addClip()
         UIColor.white.setFill()
         rounderRect.fill()
     }
+}
+
+extension PlayingCardView {
+    private struct SizeRatio {
+        static let cornerFontSizeToBoundsHeight: CGFloat = 0.085
+        static let cornerRadiusToBoundsHeight: CGFloat = 0.06
+        static let cornerOffsetToCornerRadius: CGFloat = 0.33
+        static let faceCardImageSizeToBoundsSize: CGFloat = 0.75
+    }
+    
+    private var cornerRadius: CGFloat {
+        return bounds.size.height * SizeRatio.cornerRadiusToBoundsHeight
+    }
+    
+    private var cornerOffset: CGFloat {
+        return cornerRadius * SizeRatio.cornerOffsetToCornerRadius
+    }
+    
+    private var cornerFontSize: CGFloat {
+        return bounds.size.height * SizeRatio.cornerFontSizeToBoundsHeight
+    }
+    
+    private var rankString: String {
+        switch rank {
+        case 1: return "A"
+        case 2...10: return String(rank)
+        case 11: return "J"
+        case 12: return "Q"
+        case 13: return "K"
+        default: return "?"
+        }
+    }
+}
+
+extension CGPoint {
+    func offsetBy(dx: CGFloat, dy: CGFloat) -> CGPoint{
+        return CGPoint(x: x+dx, y: y + dy)
+    }
+}
 
 
+extension CGRect {
+    func zoom(by zoomFactor: CGFloat) -> CGRect {
+        let zoomedWidth = size.width * zoomFactor
+        let zoomedHeight = size.height * zoomFactor
+        let originX = origin.x + (size.width - zoomedWidth) / 2
+        let originY = origin.y + (size.height - zoomedHeight) / 2
+        return CGRect(origin: CGPoint(x: originX,y: originY) , size: CGSize(width: zoomedWidth, height: zoomedHeight))
+    }
+    
+    var leftHalf: CGRect {
+        let width = size.width / 2
+        return CGRect(origin: origin, size: CGSize(width: width, height: size.height))
+    }
+    
+    var rightHalf: CGRect {
+        let width = size.width / 2
+        return CGRect(origin: CGPoint(x: origin.x + width, y: origin.y), size: CGSize(width: width, height: size.height))
+    }
 }
--- PlayingCards/ViewController.swift
+++ PlayingCards/ViewController.swift
@@ -13,14 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        var deck = PlayingCardDeck()
-        for _ in 1...10 {
-            if let card = deck.draw(){
-                print("card \(card)")
-            }
-            
-        }
     }
     
 }
